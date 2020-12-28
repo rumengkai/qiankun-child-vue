2f431fa56f398b33cb01ccd461176d2c3eeb8952
Ostask <502724802@qq.com>
2020-12-28 17:53:54 +0800

    去掉一些不必要的

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,22 @@
 <template>
   <div class="hello">
     child-home
-    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
-
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible"
-      width="30%"
-      :before-close="handleClose">
-      <span>这是一段信息</span>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-      </span>
-    </el-dialog>
-
-    <div class='location' style="width:600px;height:600px;" id='location'></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   data(){
     return {
-      dialogVisible: false
+      
     }
   },
   mounted(){
-    console.log(window.BMap)
-    var BMap = window.BMap
-    var map = new BMap.Map("location");    // 创建Map实例
-    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-    //添加地图类型控件
-    // map.addControl(new BMap.MapTypeControl({
-    //   mapTypes:[
-    //           BMAP_NORMAL_MAP,
-    //           BMAP_HYBRID_MAP
-    //       ]}));	  
-    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    
   },
   methods: {
-    handleClose(done) {
-      this.$confirm('确认关闭？')
-        .then(() => {
-          done();
-        })
-        .catch(() => {});
-    }
+    
   }
 }
 </script>
