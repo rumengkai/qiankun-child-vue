0d2ab9552caa692d48a0fa0db2c7814534b83443
mengkai.ru <rumengkai@gmail.com>
2021-05-06 11:33:51 +0800

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
 import Home from "../components/HelloWorld.vue"
 import List from "../components/list.vue"
 
-const packageName = require('../../package.json').name;
+const packageName = require("../../package.json").name
 
 // 判断是 qiankun 环境则增加路由前缀
-let prefix = ''
-if(window.__POWERED_BY_QIANKUN__){
-    prefix = '/'+packageName
-  }
+let prefix = ""
+if (window.__POWERED_BY_QIANKUN__) {
+  prefix = "/" + packageName
+}
 
 const routes = [
-    {
-        path:prefix + "/",
-        name:"Home",
-        component:Home
-    },
-    {
-        path:prefix + "/list",
-        name:"List",
-        component:List
-    }
+  {
+    path: prefix + "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: prefix + "/list",
+    name: "List",
+    component: List
+  }
 ]
 
 export default routes
