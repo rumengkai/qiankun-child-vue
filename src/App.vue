d1e59152c5d9f7727f955aadabd69ba89d6a4407
mengkai.ru <rumengkai@gmail.com>
2021-05-07 15:11:17 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,66 @@
 <template>
   <div id="app">
-    {{number}}
+    {{ number }}
     <div class="nav-child">
-      <router-link :to="menu.path" v-for="menu in menus" :key="menu.name">{{menu.name}}</router-link>
+      <router-link :to="menu.path" v-for="menu in menus" :key="menu.name">
+        {{ menu.name }}
+        <br />
+      </router-link>
     </div>
+    <button @click="reduce()">
+      发送消息
+    </button>
+    <br />
+    <button @click="goReactAbout()">
+      goReactAbout
+    </button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import routes from "./router"
-import {watchIndex} from "@/utils/actions";
+  import routes from "./router"
+  import { sendIndex, watchIndex } from "@/utils/actions"
 
-export default {
-  name: 'App',
-  data(){
-    return{
-      menus:routes,
-      number:0
+  export default {
+    name: "App",
+    data() {
+      return {
+        menus: routes,
+        number: 0
+      }
+    },
+    methods: {
+      reduce() {
+        this.number--
+        //改变index状态
+        sendIndex(this.number)
+      },
+      goReactAbout() {
+        console.log("~~~ goReactAbout", this.goReactAbout)
+        history.pushState(null, null, "/#/cdp-react/holmes/platform")
+      }
+    },
+    mounted() {
+      // 注册观察者函数
+      // onGlobalStateChange 第二个参数为 true，表示立即执行一次观察者函数
+      watchIndex((state, prevState) => {
+        // state: 变更后的状态; prevState: 变更前的状态
+        console.log("主应用观察者：number 改变前的值为 ", prevState.index)
+        console.log("主应用观察者：登录状态发生改变，改变后的 number 的值为 ", state.index)
+        this.number = state.index
+      }, true)
     }
-  },
-  mounted() {
-    // 注册观察者函数
-    // onGlobalStateChange 第二个参数为 true，表示立即执行一次观察者函数
-    watchIndex((state, prevState) => {
-          // state: 变更后的状态; prevState: 变更前的状态
-          console.log("主应用观察者：number 改变前的值为 ", prevState.index);
-          console.log("主应用观察者：登录状态发生改变，改变后的 number 的值为 ", state.index);
-          this.number = state.index
-      },true);
-  },
-}
+  }
 </script>
 
 <style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const packageName = require('./package.json').name;
 module.exports = {
   devServer: {
     // 监听端口
-    port: 10200,
+    port: 7001,
     // 关闭主机检查，使微应用可以被 fetch
     disableHostCheck: true,
     // 配置跨域请求头，解决开发环境的跨域问题
@@ -12,6 +12,7 @@ module.exports = {
       "Access-Control-Allow-Origin": "*",
     },
   },
+  publicPath: ".",
   configureWebpack: {
     resolve: {
       alias: {
